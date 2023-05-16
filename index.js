ecb5636217d84db817254547ffe9d64feeaca905
Julian Gruber <julian@juliangruber.com>
2023-05-16 19:29:46 +0200

    refactor, add test suite

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-import http from 'node:http'
-import { once } from 'node:events'
 import { json } from 'http-responders'
 
-const server = http.createServer((req, res) => {
+export const handle = (req, res) => {
   if (req.url === '/retrieval') {
-    json(res, { message: 'Hello World!' })
+    json(res, { hello: 'world' })
     return
   }
   res.end('Hello World!')
-})
-
-server.listen(8080)
-await once(server, 'listening')
-console.log('http://localhost:8080')
+}
