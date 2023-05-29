7a10bef97e89080c36f29b30cf1480d3f62deaaa
Julian Gruber <julian@juliangruber.com>
2023-05-29 11:17:15 +0200

    refactor

--- index.js
+++ index.js
@@ -6,46 +6,53 @@ import assert from 'http-assert'
 const handler = async (req, res, client) => {
   const segs = req.url.split('/').filter(Boolean)
   if (segs[0] === 'retrievals' && req.method === 'POST') {
-    // TODO: Consolidate to one query
-    const { rows: [retrievalTemplate] } = await client.query(`
-      SELECT id, cid, provider_address, protocol
-      FROM retrieval_templates
-      OFFSET floor(random() * (SELECT COUNT(*) FROM retrieval_templates))
-      LIMIT 1
-    `)
-    const { rows: [retrieval] } = await client.query(`
-      INSERT INTO retrievals (retrieval_template_id)
-      VALUES ($1)
-      RETURNING id
-    `, [
-      retrievalTemplate.id
-    ])
-    json(res, {
-      id: retrieval.id,
-      cid: retrievalTemplate.cid,
-      providerAddress: retrievalTemplate.provider_address,
-      protocol: retrievalTemplate.protocol
-    })
+    await createRetrieval(res, client)
   } else if (segs[0] === 'retrievals' && req.method === 'PATCH') {
-    const retrievalId = Number(segs[1])
-    assert(!Number.isNaN(retrievalId), 400, 'Invalid Retrieval ID')
-    const body = await getRawBody(req, { limit: '100kb' })
-    const { success } = JSON.parse(body)
-    const { rows } = await client.query(`
-      UPDATE retrievals
-      SET finished_at = NOW(),
-          success = $1
-      WHERE id = $2
-      RETURNING id
-    `, [
-      success,
-      retrievalId
-    ])
-    assert(rows.length > 0, 404, 'Retrieval Not Found')
-    res.end('OK')
+    await updateRetrieval(req, res, client, Number(segs[1]))
   } else {
     res.end('Hello World!')
   }
+}
+
+const createRetrieval = async (res, client) => {
+  // TODO: Consolidate to one query
+  const { rows: [retrievalTemplate] } = await client.query(`
+    SELECT id, cid, provider_address, protocol
+    FROM retrieval_templates
+    OFFSET floor(random() * (SELECT COUNT(*) FROM retrieval_templates))
+    LIMIT 1
+  `)
+  const { rows: [retrieval] } = await client.query(`
+    INSERT INTO retrievals (retrieval_template_id)
+    VALUES ($1)
+    RETURNING id
+  `, [
+    retrievalTemplate.id
+  ])
+  json(res, {
+    id: retrieval.id,
+    cid: retrievalTemplate.cid,
+    providerAddress: retrievalTemplate.provider_address,
+    protocol: retrievalTemplate.protocol
+  })
+}
+
+const updateRetrieval = async (req, res, client, retrievalId) => {
+  assert(!Number.isNaN(retrievalId), 400, 'Invalid Retrieval ID')
+  const body = await getRawBody(req, { limit: '100kb' })
+  const { success } = JSON.parse(body)
+  const { rows } = await client.query(`
+    UPDATE retrievals
+    SET finished_at = NOW(),
+        success = $1
+    WHERE id = $2
+    RETURNING id
+  `, [
+    success,
+    retrievalId
+  ])
+  assert(rows.length > 0, 404, 'Retrieval Not Found')
+  res.end('OK')
 }
 
 const errorHandler = (res, err) => {
