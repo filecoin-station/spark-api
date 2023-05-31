f567cf8643ff92d921fdd0b3ffbfc7b960355ee1
Julian Gruber <julian@juliangruber.com>
2023-05-31 12:33:48 +0200

    merge migrations, include timezone

--- migrations/1685529110405.do.add-retrieval-result-columns.sql
+++ migrations/1685529110405.do.add-retrieval-result-columns.sql
@@ -1,8 +1,8 @@
 ALTER TABLE retrieval_results
-  ADD COLUMN start_at TIMESTAMP,
+  ADD COLUMN start_at TIMESTAMP WITH TIME ZONE,
   ADD COLUMN status_code INTEGER,
-  ADD COLUMN first_byte_at TIMESTAMP,
-  ADD COLUMN end_at TIMESTAMP,
+  ADD COLUMN first_byte_at TIMESTAMP WITH TIME ZONE,
+  ADD COLUMN end_at TIMESTAMP WITH TIME ZONE,
   ADD COLUMN byte_length INTEGER;
 UPDATE retrieval_results SET start_at = NOW();
 ALTER TABLE retrieval_results ALTER COLUMN start_at SET NOT NULL;
