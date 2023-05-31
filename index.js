dda2f700673bdb9508657fb07fe85d47a69709d0
Julian Gruber <julian@juliangruber.com>
2023-05-31 15:53:17 +0200

    Add retrieval result columns (#20)
    * refactor: split up retrieval schema
    
    * add retrieval result columns
    
    * eof
    
    * docs
    
    * add validate date strings
    
    * merge migrations, include timezone
    
    * fix handling of timestamps
    
    * compare ISO strings
    
    * rename migration
    
    * clean up

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import { json } from 'http-responders'
 import { migrate } from './lib/migrate.js'
 import getRawBody from 'raw-body'
 import assert from 'http-assert'
-import toCamelCase from 'to-camel-case'
 
 const handler = async (req, res, client) => {
   const segs = req.url.split('/').filter(Boolean)
@@ -38,27 +37,51 @@ const createRetrieval = async (res, client) => {
   })
 }
 
+const validate = (obj, key, type) => {
+  if (type === 'date') {
+    const date = new Date(obj[key])
+    assert(!isNaN(date.getTime()), 400, `Invalid .${key}`)
+  } else {
+    assert.strictEqual(typeof obj[key], type, 400, `Invalid .${key}`)
+  }
+}
+
 const setRetrievalResult = async (req, res, client, retrievalId) => {
   assert(!Number.isNaN(retrievalId), 400, 'Invalid Retrieval ID')
   const body = await getRawBody(req, { limit: '100kb' })
   const result = JSON.parse(body)
+  validate(result, 'walletAddress', 'string')
+  validate(result, 'success', 'boolean')
+  validate(result, 'startAt', 'date')
+  validate(result, 'statusCode', 'number')
+  validate(result, 'firstByteAt', 'date')
+  validate(result, 'endAt', 'date')
+  validate(result, 'byteLength', 'number')
   try {
     await client.query(`
-      INSERT INTO retrieval_results
-      (retrieval_id, wallet_address, success)
-      VALUES ($1, $2, $3)
+      INSERT INTO retrieval_results (
+        retrieval_id, wallet_address, success, start_at, status_code,
+        first_byte_at, end_at, byte_length
+      )
+      VALUES (
+        $1, $2, $3, $4, $5, $6, $7,
+        $8
+      )
     `, [
       retrievalId,
       result.walletAddress,
-      result.success
+      result.success,
+      new Date(result.startAt),
+      result.statusCode,
+      new Date(result.firstByteAt),
+      new Date(result.endAt),
+      result.byteLength
     ])
   } catch (err) {
     if (err.constraint === 'retrieval_results_retrieval_id_fkey') {
       assert.fail(404, 'Retrieval Not Found')
     } else if (err.constraint === 'retrieval_results_pkey') {
       assert.fail(409, 'Retrieval Already Completed')
-    } else if (err.column) {
-      assert.fail(400, `Invalid .${toCamelCase(err.column)}`)
     } else {
       throw err
     }
