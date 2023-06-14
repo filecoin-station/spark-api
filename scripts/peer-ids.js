9a199bebc94dec0d13cd3b5549e609064ec74e44
Julian Gruber <julian@juliangruber.com>
2023-06-14 13:41:06 +0200

    fix find peer id

--- scripts/peer-ids.js
+++ scripts/peer-ids.js
@@ -5,17 +5,36 @@ const { DATABASE_URL } = process.env
 const client = new pg.Client({ connectionString: DATABASE_URL })
 await client.connect()
 
+const notFound = (templateId) =>
+  `UPDATE retrieval_templates SET peer_id = 'not found', enabled = FALSE WHERE id = ${templateId};`
+
+const findProviderResult = (template, body) => {
+  for (const multiHashResult of body.MultihashResults) {
+    for (const providerResult of multiHashResult.ProviderResults) {
+      if (providerResult.Provider.Addrs.includes(template.provider_address)) {
+        return providerResult
+      }
+    }
+  }
+}
+
 const {
   rows: retrievalTemplates
 } = await client.query('SELECT * FROM retrieval_templates')
 for (const template of retrievalTemplates) {
   const res = await fetch(`https://cid.contact/cid/${template.cid}`)
   if (res.status === 404) {
-    console.log(`UPDATE retrieval_templates SET peer_id = 'not found', enabled = FALSE WHERE id = ${template.id};`)
+    console.log(notFound(template.id))
   } else if (res.ok) {
     const body = await res.json()
-    template.peerID = body.MultihashResults[0].ProviderResults[0].Provider.ID
-    console.log(`UPDATE retrieval_templates SET peer_id = '${template.peerID}' WHERE id = ${template.id};`)
+    const providerResult = findProviderResult(template, body)
+    if (providerResult) {
+      console.log(
+        `UPDATE retrieval_templates SET peer_id = '${providerResult.Provider.ID}' WHERE id = ${template.id};`
+      )
+    } else {
+      console.log(notFound(template.id))
+    }
   } else {
     throw new Error(`${res.status}: ${await res.text()}`)
   }
