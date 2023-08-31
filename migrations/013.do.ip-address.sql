23bb2e8b198e260aba2d712691e73a5df3c502c9
Miroslav Bajtoš <oss@bajtos.net>
2023-08-31 13:20:29 +0200

    fixup! fix type on column name
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- migrations/013.do.ip-address.sql
+++ migrations/013.do.ip-address.sql
@@ -1,6 +1,6 @@
 ALTER TABLE retrievals ADD COLUMN created_from_address INET;
 UPDATE retrievals SET created_from_address = '0.0.0.0';
-ALTER TABLE retrievals ALTER COLUMN created_at_round SET NOT NULL;
+ALTER TABLE retrievals ALTER COLUMN created_from_address SET NOT NULL;
 
 ALTER TABLE retrieval_results ADD COLUMN completed_from_address INET;
 UPDATE retrieval_results SET completed_from_address = '0.0.0.0';
