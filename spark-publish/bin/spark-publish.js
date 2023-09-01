541afd54ed8a85cc7497cfc693e34dcac64debc6
Julian Gruber <julian@juliangruber.com>
2023-09-01 15:07:30 +0200

    clean up (pkg.version not being used)

--- spark-publish/bin/spark-publish.js
+++ spark-publish/bin/spark-publish.js
@@ -1,5 +1,4 @@
 import fs from 'node:fs/promises'
-import { join, dirname } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import pg from 'pg'
 import { startPublishLoop } from '../index.js'
@@ -18,20 +17,8 @@ const {
   WEB3_STORAGE_API_TOKEN
 } = process.env
 
-const pkg = JSON.parse(
-  await fs.readFile(
-    join(
-      dirname(fileURLToPath(import.meta.url)),
-      '..',
-      'package.json'
-    ),
-    'utf8'
-  )
-)
-
 Sentry.init({
   dsn: 'https://4a55431b256641f98f6a51651526831f@o1408530.ingest.sentry.io/4505199717122048',
-  release: pkg.version,
   environment: SENTRY_ENVIRONMMENT,
   tracesSampleRate: 0.1
 })
