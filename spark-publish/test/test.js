98601f98bbd0250b497adb5e7cb92a125809bcd5
Julian Gruber <julian@juliangruber.com>
2023-09-02 13:25:09 +0200

    add basic test

--- spark-publish/test/test.js
+++ spark-publish/test/test.js
@@ -1,5 +1,62 @@
+import { publish } from '../index.js'
+import assert from 'node:assert'
+import { CID } from 'multiformats/cid'
+
 describe('publish', () => {
-  it('publishes', () => {
-    // TODO
+  it('publishes', async () => {
+    const client = {
+      async query (_, params) {
+        assert.strictEqual(params[0], 1)
+
+        client.query = (_, params) => {
+          assert.strictEqual(
+            params[0].toString(),
+            'bafybeicmyzlxgqeg5lgjgnzducj37s7bxhxk6vywqtuym2vhqzxjtymqvm'
+          )
+          assert.deepStrictEqual(params[1], [])
+        }
+
+        return { rows: [] }
+      }
+    }
+
+    const web3Storage = {
+      async put (files) {
+        assert.strictEqual(files.length, 1)
+        return CID.parse(
+          'bafybeicmyzlxgqeg5lgjgnzducj37s7bxhxk6vywqtuym2vhqzxjtymqvm'
+        )
+      }
+    }
+
+    const ieContract = {
+      async addMeasurements (cid) {
+        assert.strictEqual(
+          cid,
+          'bafybeicmyzlxgqeg5lgjgnzducj37s7bxhxk6vywqtuym2vhqzxjtymqvm'
+        )
+        return {
+          async wait () {
+            return {
+              events: [
+                {
+                  event: 'MeasurementsAdded',
+                  args: {
+                    roundIndex: 1
+                  }
+                }
+              ]
+            }
+          }
+        }
+      }
+    }
+
+    await publish({
+      client,
+      web3Storage,
+      ieContract,
+      maxMeasurements: 1
+    })
   })
 })
