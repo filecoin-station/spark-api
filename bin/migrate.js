fbb449251417ca542afb3a3c0337b6e08116193f
Julian Gruber <julian@juliangruber.com>
2023-09-02 14:27:39 +0200

    fix connect

--- bin/migrate.js
+++ bin/migrate.js
@@ -4,5 +4,6 @@ import pg from 'pg'
 const { DATABASE_URL } = process.env
 
 const client = new pg.Client({ connectionString: DATABASE_URL })
+await client.connect()
 await migrate(client)
 await client.end()
