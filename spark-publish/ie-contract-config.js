03af30c4134955d3eba78a2a18d5e92ff5dfde75
Julian Gruber <julian@juliangruber.com>
2023-09-26 14:52:13 +0200

    update contract

--- spark-publish/ie-contract-config.js
+++ spark-publish/ie-contract-config.js
@@ -2,7 +2,7 @@ import fs from 'node:fs/promises'
 import { fileURLToPath } from 'node:url'
 
 const {
-  IE_CONTRACT_ADDRESS = '0x816830a1e536784ecb37cf97dfd7a98a82c86643',
+  IE_CONTRACT_ADDRESS = '0x381b50e8062757c404dec2bfae4da1b495341af9',
   RPC_URL = 'https://api.calibration.node.glif.io/rpc/v0'
 } = process.env
 
