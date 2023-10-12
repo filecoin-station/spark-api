0dd471f9ee14f4f0716ed9d3e796aaa3e4cd0340
Miroslav Bajtoš <oss@bajtos.net>
2023-10-12 17:09:35 +0200

    fix: sentry environment config in production (#105)
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- bin/spark.js
+++ bin/spark.js
@@ -13,7 +13,7 @@ const {
   PORT = 8080,
   HOST = '127.0.0.1',
   DATABASE_URL,
-  SENTRY_ENVIRONMMENT = 'development'
+  SENTRY_ENVIRONMENT = 'development'
 } = process.env
 
 const pkg = JSON.parse(
@@ -30,7 +30,7 @@ const pkg = JSON.parse(
 Sentry.init({
   dsn: 'https://4a55431b256641f98f6a51651526831f@o1408530.ingest.sentry.io/4505199717122048',
   release: pkg.version,
-  environment: SENTRY_ENVIRONMMENT,
+  environment: SENTRY_ENVIRONMENT,
   tracesSampleRate: 0.1
 })
 
