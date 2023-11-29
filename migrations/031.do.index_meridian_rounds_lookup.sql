46a219bdea6eb4a4fb566ec7e516abe2aed3c7be
Miroslav Bajtoš <oss@bajtos.net>
2023-11-29 10:10:56 +0100

    perf: index meridian & spark round lookups
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

---
+++ migrations/031.do.index_meridian_rounds_lookup.sql
@@ -0,0 +1,2 @@
+CREATE INDEX CONCURRENTLY meridian_contract_versions_contract_address
+ON meridian_contract_versions (contract_address);
---
+++ migrations/032.do.index_spark_round_timestamps.sql
@@ -0,0 +1,1 @@
+CREATE INDEX CONCURRENTLY spark_rounds_created_at ON spark_rounds (created_at);
