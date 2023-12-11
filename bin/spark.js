67c5549d3f2381cdc7f155030212d23a6d58db61
Miroslav Bajtoš <oss@bajtos.net>
2023-12-11 09:15:25 +0100

    feat: keep up to 100 open connections in pg.Pool
    - spark-db is configured with max-connections=1000
    - we have four instances of spark-api
    - we must allow other services (notably spark-publish) to connect too
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- bin/spark.js
+++ bin/spark.js
@@ -41,6 +41,10 @@ const client = new pg.Pool({
   connectionString: DATABASE_URL,
   // allow the pool to close all connections and become empty
   min: 0,
+  // this values should correlate with service concurrency hard_limit configured in fly.toml
+  // and must take into account the connection limit of our PG server, see
+  // https://fly.io/docs/postgres/managing/configuration-tuning/
+  max: 100,
   // close connections that haven't been used for one second
   idleTimeoutMillis: 1000,
   // automatically close connections older than 60 seconds
