925c437cffc369807e6d21d507006e20dd05d6f8
Julian Gruber <julian@juliangruber.com>
2024-01-15 14:08:12 +0100

    publish:fly: remove `services` section

--- spark-publish/bin/spark-publish.js
+++ spark-publish/bin/spark-publish.js
@@ -7,7 +7,6 @@ import { spawn } from 'node:child_process'
 import { once } from 'events'
 import { fileURLToPath } from 'node:url'
 import { rpcUrls } from '../ie-contract-config.js'
-import http from 'node:http'
 
 const {
   SENTRY_ENVIRONMENT = 'development',
@@ -38,9 +37,6 @@ console.log(
   signer.address,
   newDelegatedEthAddress(signer.address, 'f').toString()
 )
-
-// FIXME fly.io
-http.createServer((_, res) => res.end()).listen(8080)
 
 let rpcUrlIndex = 0
 
