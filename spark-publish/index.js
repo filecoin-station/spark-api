aac1b375c4930d4f88f552c750104314dd5bd8f0
Miroslav Bajtoš <oss@bajtos.net>
2024-01-31 15:32:13 +0100

    fix: delete measurements from inside transaction
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- spark-publish/index.js
+++ spark-publish/index.js
@@ -3,14 +3,14 @@
 import { record } from './lib/telemetry.js'
 
 export const publish = async ({
-  client,
+  client: pgPool,
   web3Storage,
   ieContract,
   maxMeasurements = 1000,
   logger = console
 }) => {
   // Fetch measurements
-  const { rows: measurements } = await client.query(`
+  const { rows: measurements } = await pgPool.query(`
     SELECT
       id,
       spark_version,
@@ -38,7 +38,7 @@ export const publish = async ({
   // Fetch the count of all unpublished measurements - we need this for monitoring
   // Note: this number will be higher than `measurements.length` because spark-api adds more
   // measurements in between the previous and the next query.
-  const totalCount = (await client.query(
+  const totalCount = (await pgPool.query(
     'SELECT COUNT(*) FROM measurements'
   )).rows[0].count
 
@@ -65,12 +65,12 @@ export const publish = async ({
   const ieAddMeasurementsDuration = new Date() - start
   logger.log('Measurements added to round', roundIndex.toString())
 
-  const pgClient = await client.connect()
+  const pgClient = await pgPool.connect()
   try {
     await pgClient.query('BEGIN')
 
     // Delete published measurements
-    await client.query(`
+    await pgClient.query(`
       DELETE FROM measurements
       WHERE id = ANY($1::int[])
     `, [
@@ -91,7 +91,7 @@ export const publish = async ({
     pgClient.release()
   }
 
-  await client.query('VACUUM measurements')
+  await pgPool.query('VACUUM measurements')
 
   // TODO: Add cleanup
   // We're not sure if we're going to stick with web3.storage, or switch to
