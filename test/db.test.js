a1f58ca7ddb4c95e3a3741d5a017511369463da2
Miroslav Bajtoš <oss@bajtos.net>
2024-05-28 13:48:07 +0200

    test: clean DB before testing retrievable_deals PK
    I noticed that subsequent runs of this test failed because the test
    was trying to insert the same CID again.
    
        duplicate key value violates unique constraint "retrievable_deals_pkey"
    
    This commit fixes the problem by modifying the test to remove dummy
    deals created by previous runs.
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- test/db.test.js
+++ test/db.test.js
@@ -20,11 +20,14 @@ describe('spark-api database', () => {
   })
 
   it('allows multiple storage deals for the same CID', async () => {
+    const DUMMY_CID = 'bafyone'
+    await client.query('DELETE FROM retrievable_deals WHERE cid = $1', [DUMMY_CID])
+
     await client.query(`
       INSERT INTO retrievable_deals (cid, miner_id, expires_at)
       VALUES ($1, $2, $3), ($1, $4, $3)
     `, [
-      'bafyone',
+      DUMMY_CID,
       'f010',
       new Date(),
       'f020'
