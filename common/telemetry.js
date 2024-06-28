fad4088dfe7375e38ae4d81c9e870f49e699c00b
Miroslav Bajtoš <oss@bajtos.net>
2024-06-28 09:21:34 +0200

    feat: process.env.INFLUXDB_TOKEN
    Move the configuration of the writer token from plain-text code
    to Fly.io secrets.
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- common/telemetry.js
+++ common/telemetry.js
@@ -1,10 +1,18 @@
 import { InfluxDB, Point } from '@influxdata/influxdb-client'
 
+const {
+  INFLUXDB_TOKEN
+} = process.env
+
+if (!INFLUXDB_TOKEN) {
+  console.warn('Warning: INFLUXDB_TOKEN was not provided by the environment')
+}
+
 const influx = new InfluxDB({
   url: 'https://eu-central-1-1.aws.cloud2.influxdata.com',
-  // spark-publish-write
-  token: 'Zkqa_s7mI0W_WKI6DUmu-iRnQkCvwNaQfbPK_zT7I6iYYaC2C1kokdlhO2jb4tjRcAJQHQXAGnrdD3vqlMZ63g=='
+  token: INFLUXDB_TOKEN
 })
+
 const publishWriteClient = influx.getWriteApi(
   'Filecoin Station', // org
   'spark-publish', // bucket
