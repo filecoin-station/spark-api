aaa852b115e164085189b6b4a78b61041add7bc6
Miroslav Bajtoš <oss@bajtos.net>
2025-01-09 13:49:24 +0100

    feat: add table `eligible_deals` (#485)
    * feat: add table `eligible_deals`
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>
    
    * fixup! add 10k eligible deals to seed the new table
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>
    
    * Update migrations/062.do.eligible-deals.sql
    
    Co-authored-by: Julian Gruber <julian@juliangruber.com>
    
    * fixup! reformat SQL comment
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>
    
    ---------
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>
    Co-authored-by: Julian Gruber <julian@juliangruber.com>

---
+++ migrations/062.do.eligible-deals.sql
@@ -0,0 +1,18 @@
+-- After the seed migration `063.do.eligible-deals-seed.sql`,
+-- this table's content will be managed by
+-- https://github.com/filecoin-station/fil-deal-ingester
+CREATE TABLE eligible_deals (
+  miner_id TEXT NOT NULL,
+  client_id TEXT NOT NULL,
+  piece_cid TEXT NOT NULL,
+  piece_size BIGINT NOT NULL,
+  payload_cid TEXT NOT NULL,
+  expires_at TIMESTAMPTZ NOT NULL,
+
+  sourced_from_f05_state BOOLEAN NOT NULL DEFAULT TRUE,
+
+  PRIMARY KEY (miner_id, client_id, piece_cid, piece_size)
+);
+
+CREATE INDEX eligible_deals_miner_id ON eligible_deals (miner_id);
+CREATE INDEX eligible_deals_client_id ON eligible_deals (client_id);
